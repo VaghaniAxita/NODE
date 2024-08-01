ae4a20203b2b4dd4d41e88bd2c481996e2211edf
VaghaniAxita <vaghaniaxita23@gmail.com>
2024-08-01 21:27:32 +0530

    crud opreration

--- DATABASE/index.js
+++ DATABASE/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const dbConnect = require("./db")
 const User = require("./user.schema")
+const isValid = require("./validate")
 
 const app = express()
 
@@ -15,6 +16,22 @@ app.get("/", async (req, res) => {
 //post for creating
 app.post("/", async (req, res) => {
     let data = await User.create(req.body)
+    res.send(data)
+})
+
+//delete
+app.delete("/:id",async(req,res)=>{
+    // console.log(req.params);
+    let{id}=req.params;
+    // console.log(id);
+    let data = await User.findByIdAndDelete(id)
+    res.send("delete")
+})
+
+//update
+app.path("/:id",isValid, async(req,res)=>{
+    let{id}=req.params;
+    let data=await User.findByIdAndUpdate(id,req.body)
     res.send(data)
 })
 
